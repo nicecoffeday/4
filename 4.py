6eb98f95928a0da600d3fbcf46ff0e418cfbadee
ChihWen <heart3940@gmail.com>
2020-09-28 22:56:12 +0800

    change while i > 0

--- 4.py
+++ 4.py
@@ -1,6 +1,6 @@
 passward = '123456'
 i = 3 
-while True:
+while i > 0:
 	pwd = input('請輸入密碼: ')
 	if pwd == passward:
 		print('輸入正確')
@@ -8,5 +8,3 @@ while True:
 	else:
 		i = i - 1
 		print('密碼錯誤', i, '次機會')
-		if i == 0:
-			break
